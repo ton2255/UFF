ef864115640de98221b3456b2301b1af8598e0a9
ton2255 <toon@hotmail.com.br>
2017-01-28 22:24:12 -0200

    Update 1021.py

--- 1021.py
+++ 1021.py
@@ -27,6 +27,3 @@ if 0 <= valor <= 1000000.00:
     print(cincoCent, "moeda(s) de R$ 0.05")
     umCent = int(valor % 100 % 50 % 20 % 10 % 5 % 2 % 1 % 0.5 % 0.25 % 0.10 % 0.05 / 0.01)
     print(umCent, "moeda(s) de R$ 0.01")
-else:
-    
-
